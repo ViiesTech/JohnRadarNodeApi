f8bf32ce42aea3ff53543c2a524cdc8a6b4d930b
ApexifyApps <141789096+ApexifyApps@users.noreply.github.com>
2023-09-14 02:07:59 +0500

    Atlast Connected

--- ConfigDatabase/Database.js
+++ ConfigDatabase/Database.js
@@ -6,7 +6,7 @@ const ConnectDataBase = () => {
 
 
 mongoose.set('strictQuery', true);
-mongoose.connect('mongodb://localhost:27017/JohnRadar');
+mongoose.connect('mongodb+srv://visstechapps:qL5Df46pN91hAu97@viistech.r6nztgu.mongodb.net/JohnRadar');
 const db = mongoose.connection;
 db.on("error",(error)=>console.log(error));
 db.once("open",()=>console.log("DB Connected"));
